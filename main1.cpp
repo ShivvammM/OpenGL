8a2f2c85a722fda08c668cafa2b9371992606990
Shivam Marvadi <Shivam.Marvadi@einfochips.com>
2025-02-19 14:58:09 +0530

    Initial Commit

---
+++ main1.cpp
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <cmath>
+using namespace std;
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
+#include "Window.h"
+#include "Mesh.h"
+#include "Camera.h"
+#include "texture.h"
+#include "Light.h"
+
